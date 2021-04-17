4f1da8212da624a4cf4c4027bfdfbd84e0270d66
deradler <stefan.schoening@gmx.de>
2021-04-17 17:00:58 +0200

    8

--- lib/buttons_tabbar.dart
+++ lib/buttons_tabbar.dart
@@ -283,7 +283,7 @@ class _ButtonsTabBarState extends State<ButtonsTabBar>
                 : Container(),
             SizedBox(
               width: tab.icon == null || (tab.text == null && tab.child == null)
-                  ? 0
+                  ? 8
                   : widget.labelSpacing,
             ),
             tab.text != null
